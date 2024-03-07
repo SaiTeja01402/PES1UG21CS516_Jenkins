5fa4e6013648861832bf4c856ae19fae6b87b52f
SaiTeja01402 <teja.yanamandra@gmail.com>
2024-03-07 21:53:42 +0530

    initial commit

--- Jenkins_lab-main/main/hello.cpp
+++ Jenkins_lab-main/main/hello.cpp
@@ -6,6 +6,5 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << " I have added a webhook successfully\n";
   return 0;
 }
